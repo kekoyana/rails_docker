4a10b5c0e48c1f2adbd73077da28c1e5d6dbc003
kekoyana <kekoyana0@gmail.com>
2020-01-30 01:05:14 +0900

    scaffold abilities

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_155656) do
+ActiveRecord::Schema.define(version: 2020_01_29_160402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "abilities", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "user_positions", force: :cascade do |t|
     t.bigint "user_id", null: false
