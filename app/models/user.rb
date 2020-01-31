f11875cb9c76250b027f9cbc25a3e826cd274438
kekoyana <kekoyana0@gmail.com>
2020-02-01 01:24:23 +0900

    sample

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,21 @@
 class User < ApplicationRecord
-  has_many :user_abilities
+  has_many :user_abilities, dependent: :destroy
   has_many :abilities, through: :user_abilities
+  has_many :user_positions, dependent: :destroy
   validates :name, presence: true
+
+  def positions
+    Position.where(id: position_ids)
+  end
+
+  def position_ids
+    user_positions.pluck(:position_id)
+  end
+
+  def position_ids=(ids)
+    self.user_positions = ids.map do |id|
+      self.user_positions.new(position_id: id)
+    end
+    ids
+  end
 end
--- app/models/user_position.rb
+++ app/models/user_position.rb
@@ -1,3 +1,5 @@
 class UserPosition < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :position
 end
